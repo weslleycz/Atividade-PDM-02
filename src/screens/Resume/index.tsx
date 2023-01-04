6fa7c3f6415db5934871ce999ab086a04ee31099
Weslley <weslleysouza22@gmail.com>
2023-01-04 01:09:54 -0300

    ✨ Tela de categoria.

--- src/screens/Resume/index.tsx
+++ src/screens/Resume/index.tsx
@@ -1,5 +1,6 @@
 import { ScrollView } from "native-base";
 import React from "react";
+import { Categories } from "../../components/Categories";
 import { Footer } from "../../components/Footer";
 import { Header } from "../../components/Header";
 import { Title } from "../../components/Title";
@@ -12,8 +13,9 @@ export const Resume = ({ navigation }: NavigationProps) => {
                 <Header>
                     <Title text="Resumo por categoria" />
                 </Header>
+                <Categories />
             </ScrollView>
-            <Footer  page="resume" navigation={navigation} />
+            <Footer page="resume" navigation={navigation} />
         </>
     );
 };
